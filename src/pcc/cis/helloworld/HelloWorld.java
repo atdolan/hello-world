5fd4279c3f80824a6d4c7593dc5bee68ad8a3d43
Aanund Dolan <aanund.dolan@pcc.edu>
2016-10-25 20:28:41 -0700

    added main method that prints hello world

--- src/pcc/cis/helloworld/HelloWorld.java
+++ src/pcc/cis/helloworld/HelloWorld.java
@@ -4,4 +4,7 @@ package pcc.cis.helloworld;
  * Created by Aanund on 10/25/2016.
  */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello, World!");
+    }
 }
